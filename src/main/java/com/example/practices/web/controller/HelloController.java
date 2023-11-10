eefa38037c211b3083839a61620d11ff13e6e82f
Gerald Gonzalez <gedgonz7@gmail.com>
2023-11-10 16:15:22 -0600

    was move controller

--- src/main/java/com/example/practices/web/controller/HelloController.java
+++ src/main/java/com/example/practices/web/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.practices;
+package com.example.practices.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
